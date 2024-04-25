b8f271ca04fc6cfac3cbd36c84b7ccd451f1f5d8
Sreeni <kattubadi426@gmail.com>
2024-04-25 09:12:58 +0530

    Initial commit

---
+++ slicing/Negative Index.py
@@ -0,0 +1,12 @@
+# str='ETL'
+#
+# print(f"last character available in {str}",str[-1])
+# print(f"last 2nd character available in {str}",str[-2])
+# print(f"last 3rd character available in {str}",str[-3])
+
+# print(f"last 4th character available in {str}",str[-4])
+
+
+str= 'ETL AUTOMATION'
+
+print("available  is",str[3:-2:1])
---
+++ slicing/string_slicing.py
@@ -0,0 +1,28 @@
+
+ # "str = 'ETL'
+
+# print("The value is ",str)
+#
+# print(f"first character available {str}",str[0])
+# print(f"second character available {str}",str[1])
+# print(f"third character available {str}",str[2])
+
+# print(f"fourth character available {str}",str[2])
+
+# for i in range(len(str)):
+#     print(f"The value of index {i} and value is",str[i])
+
+# str2= 'ETL AUTOMATION'
+#
+# print(f" 0:4 available in the {str} is",str2[0:5])   # end= end-1
+# print(f" 0:4 available in the {str} is",str2[4:])
+# # print(f" all values in the {str} is",str[ : ])
+# print(f" 0:4 available in the {str} is",str2[:4])
+
+email='seenu@gmail.com'
+
+print(f"user from {email}is",email[0:email.find('@')])
+
+print(f"user from {email}is",email[email.find('@'):
+                             ])
+
