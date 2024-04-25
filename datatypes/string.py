b9e334ec9f4e648b0e8c94e13949d7503af78b45
Sreeni <kattubadi426@gmail.com>
2024-04-25 08:46:14 +0530

    Initial commit

--- datatypes/string.py
+++ datatypes/string.py
@@ -2,133 +2,169 @@
 created by Sreeni on 04/23/2024
 """
 
-string = 'ETL'
+# string = 'ETL'
+#
+# print("The value of string is ", string)
+# print("type of string is ", type(string))
+# print("Memory of string", id(string))
+#
+# string2 = "ETL"
+#
+# print("The value of string2 is ", string2)
+# print("type of string2 is ", type(string2))
+# print("Memory of string2", id(string2))
+#
+# string3 = """ETL"""
+#
+# print("The value of string3 is ", string3)
+# print("type of string3 is ", type(string3))
+# print("Memory of string3", id(string3))
+#
+# string4 = '''ETL'''
+#
+# print("The value of string4 is ", string4)
+# print("type of string4 is ", type(string4))
+# print("Memory of string4", id(string4))
+#
+# string = " I don't care "
+#
+# print("The value of string is ", string)
+# print("type of string is ", type(string))
+# print("Memory of string", id(string))
+#
+# string = ' I don"t care '
+#
+# print("The value of string is ", string)
+# print("type of string is ", type(string))
+# print("Memory of string", id(string))
+#
+# string = """i don't care """
+# string = '''i don"t care '''
+# etl_string= '''ETL in testing means an extract, transform and load process that reads data from multiple source systems,
+#  transports it to a data transformation layer for further processing which includes cleaning,
+# consolidating, integrating, and then loading into a target database or file'''
+#
+# print("The value of etl_string is ", etl_string)
+# print("type of etl_string is ", type(etl_string))
+# print("Memory of etl_string", id(etl_string))
+#
+#
+#
+# print("methods available in string datatype",dir(etl_string))
+#
+# str7 = 'ETL Automation testing'
+# print("Before capitalize str7 is ", str7)
+# print("After capitalize str7 is", str7.capitalize())
+# print("After title str7 is", str7.title())
+# #
+#
+# print("Before casefold str7 is ", str7)
+# print("After casefold str7 is", str7.casefold())
+# print("After lower str7 is", str7.lower())
+# print("After swapcase str7 is", str7.swapcase())
+# print("After upper str7 is", str7.upper())
+#
+#
+# txt = "ETL Testing"
+# x = txt.center(100)
+# print(x)
+#
+# str8 = 'BIG DATA TESTING 1| 4342 [472534623542 '
+# print(f"count of A in {str8}", str8.count('A'))
+# print(f"count of | in {str8}", str8.count('|',20,30))
+#
+# name = 'Sreeni'
+# print(f"count of en in {name} and n ", name.count('en'),name.count('n'))
 
-print("The value of string is ", string)
-print("type of string is ", type(string))
-print("Memory of string", id(string))
 
-string2 = "ETL"
 
-print("The value of string2 is ", string2)
-print("type of string2 is ", type(string2))
-print("Memory of string2", id(string2))
 
-string3 = """ETL"""
 
-print("The value of string3 is ", string3)
-print("type of string3 is ", type(string3))
-print("Memory of string3", id(string3))
+# print("*"*50)
 
-string4 = '''ETL'''
+# str = input("enter str value:") # input, raw_input
 
-print("The value of string4 is ", string4)
-print("type of string4 is ", type(string4))
-print("Memory of string4", id(string4))
+# print(f"str.startswith('Auto') in {str} : ",str.startswith('Auto'))
+#
+# print(f"str.startswith('Auto') in {str} : ",str.startswith('auto'))
+#
+# print("Ends with Testing ",str.endswith('Testing'))
 
-string = " I don't care "
+# print(help(str.startswith))
+# print(help(str.endswith))
 
-print("The value of string is ", string)
-print("type of string is ", type(string))
-print("Memory of string", id(string))
+# str ='TESTING'
+# print("str.find('Test')",str.find(' Test'))
+# print("str.find('i')",str.find('i'))
+# #
+# print("str.find('i',8)",str.find('i',8))
+#
+# print("str.find('i',8,12)",str.find('i',8,12))
 
-string = ' I don"t care '
+# name = 'Prashant'
+# age = 24
+# sname = 'K'
+# #
+# print(f"name is {name} and age is {age}")
+# print("name is {first_name} and age is {person_age}".format(person_age= age,first_name=name))
+# print("firstname is {} and lastname is {} and age {} ".format(name,sname,age))
 
-print("The value of string is ", string)
-print("type of string is ", type(string))
-print("Memory of string", id(string))
 
-string = """i don't care """
-string = '''i don"t care '''
-etl_string= '''ETL in testing means an extract, transform and load process that reads data from multiple source systems,
- transports it to a data transformation layer for further processing which includes cleaning,
-consolidating, integrating, and then loading into a target database or file'''
+# schema = input("enter schema:")
+# table = input("enter table name:")
+# second_table = input("enter second table:")
+# key_column = input("enter key column table:")
+# age = input("enter age :")
+#
+# query = f"""select count(1) from {schema}.{table} a inner join {schema}.{second_table} b
+#          on a.{key_column}=b.{key_column} where age<{age} """
+#
+# print(query)
+# for i in env:
+#     for j in table:
+#         #print(f"select * from {i}.{j}")
+#         print("select * from {}.{}".format(i,j))
 
-print("The value of etl_string is ", etl_string)
-print("type of etl_string is ", type(etl_string))
-print("Memory of etl_string", id(etl_string))
-
-print(dir(etl_string))
-
-print("methods available in string datatype",dir(etl_string))
-
-str7 = 'ETL AUTOMATION'
-print("Before capitalize str7 is ", str7)
-print("After capitalize str7 is", str7.capitalize())
-
-print("Before casefold str7 is ", str7)
-print("After casefold str7 is", str7.casefold())
-
-print("Before lower str7 is ", str7)
-print("After lower str7 is", str7.lower())
-
-txt = "banana"
-x = txt.center(10)
-print(x)
-str8 = 'BIG DATA TESTING'
-print(f"count of e in {str8}", str8.count('a'))
-
-print("*"*50)
-
-str = "Automation Testing"
-
-print("str.startswith('Auto')",str.startswith('Auto'))
-print("str.startswith('auto')",str.startswith('auto'))
-print("Ends with Testing ",str.endswith('Testing'))
-print("str.endswith('testing')",str.endswith('testing'))
-
-print("str.find('Test')",str.find('Test'))
-
-print("str.find('i')",str.find('i'))
-
-print("str.find('i',8)",str.find('i',8))
-
-print("str.find('i',8,12)",str.find('i',8,12))
-
-name = 'Hari'
-age = 24
-
-print("My name is {name} and age is {age}".format(name=name,age= age))
-name = 'Sreeni'
-age = 25
-print(f"My name is {name} and age is {age}")
-
-env = ['dev', 'qa']
-table = ['emp', 'dept']
-for i in env:
-    for j in table:
-    #print(i)
-        print(f"select * from {i}.{j}")
-
-str = 'ETLbatch3'
+str = 'ETLbatch5'
 
 print(f"str.isalnum() on {str}", str.isalnum())
-str2 = 'ETL_batch3'
+
+str2 = 'ETL batch 3'
 print(f"str2.isalnum() on {str2}", str2.isalnum())
+#
+# str3 = 'ETLbatch'
+# print(f"str3.isalnum() on {str3}", str3.isalpha())
+#
+# str4 = 'ETLbatch1'
+# print(f"str4.isalnum() on {str4}", str4.isalpha())
+#
+# str = ' Sreeni Kattu  '
+# print("length of str before strip", len(str))
+# print(f"str.strip()", str.strip())
+# print("length of str after strip", len(str.strip()))
+#
+# print(f"str.rstrip()", str.lstrip())
+# print("length of str after lstrip", len(str.lstrip()))
+# print(f"str.lstrip()", str.lstrip())
+# print("length of str after rstrip", len(str.rstrip()))
+# str7 = 'ETL Automation'
+# print(f"str7.lstrip()", str7.rstrip('Automation'))
+# str8 = 'ETL Automation'
+# print(f"str8.lstrip()", str8.lstrip('ETL'))
+#
+# str9 = 'ETL-Bigdata-testing-automation'
+# str10 = 'ETL,Bigdata,testing,automation'
+#
+# print('str9.split("-",2)',str9.split("-"))
+# print('str10.split(",",2)',str10.split(","))
+# print('type str10.split(",",2)',type(str10.split(",")))
 
-str3 = 'ETLbatch'
-print(f"str3.isalnum() on {str3}", str3.isalpha())
+a = "MyFolder"
+b = "Demo002"
+c = "2bring"
+d = "my demo"
 
-str4 = 'ETLbatch1'
-print(f"str4.isalnum() on {str4}", str4.isalpha())
-
-str = ' Sreeni Kattu  '
-print("length of str before strip", len(str))
-print(f"str.strip()", str.strip())
-print("length of str after strip", len(str.strip()))
-
-print(f"str.rstrip()", str.lstrip())
-print("length of str after lstrip", len(str.lstrip()))
-print(f"str.lstrip()", str.lstrip())
-print("length of str after rstrip", len(str.rstrip()))
-str7 = 'ETL Automation'
-print(f"str7.lstrip()", str7.rstrip('Automation'))
-str8 = 'ETL Automation'
-print(f"str8.lstrip()", str8.lstrip('ETL'))
-
-str9 = 'ETL-Bigdata-testing-automation'
-str10 = 'ETL,Bigdata,testing,automation'
-
-print('str9.split("-",2)',str9.split("-"))
-print('str10.split(",",2)',str10.split(","))
-print('type str10.split(",",2)',type(str10.split(",")))
+print(a.isidentifier())
+print(b.isidentifier())
+print(c.isidentifier())
+print(d.isidentifier())
