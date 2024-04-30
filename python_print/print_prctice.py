b0fc2673950fb5052ab1a53e225a897fc49ff30f
Prashant_C <etldev2023@gmail.com>
2024-04-30 08:12:39 +0530

    Merge remote-tracking branch 'origin/April_python_Prashant' into April_python_Prashant

--- python_print/print_prctice.py
+++ python_print/print_prctice.py
@@ -5,9 +5,42 @@ print("hello world","hi")
 a = 10
 b = 20
 c = 0
+d =  2
 print("a")
 print("a","b")
 print(a,b)
-print("value of a is : ",a, "value of b is : ",b)
+print("value of a is : ", a, "value of b is : ", b)
+print("hello world" , "well come ETL Automation")
+print("Prashant","C")
+print("Prashant","D","C")
+print(a)
+print(a,b,c)
+print(a,b, sep='-')
+e = 9845988324
+print(e, sep='-') # not getting output as expected
+print(a,b,c,c, sep='@')
+print("hello world", "well come ETL Automation", sep='-')
+print("hello world","well come ETL Automation", sep='\t')  # not getting output as expected
+print("hello world", "well come ETL Automation", sep='---')
+print("Prashant","D","C", sep='-')
+print("Prashant","D","C", sep='     ')
+print(a,b,c,c, sep=' -@# ')
+print("sum a,b,c,d,e",a+b+c+d+e) # not getting output as expected
+print("sum a,b,c,d : ",a+b+c+d)
+print("sub,a,b : ", a-b)
+print("multiplied  a,b : ", a * b)
+print("divided by a,b : ", a / b)
+print("sum values of a,b is : ", a+b, "and", "sub values of a,b is : ", a-b) #---------
+print("-"*50)
+print("*"*100)
+
+print("value of a is :",a , end='\t')
+print("value of b is :",b , end='\t')
+print("value of c is :",c , end='\t')
+
+print("Prashant",Prashant,end='\t') # not getting the output
+
+
+
 
 
