e58f33f2fbf015f8328c8f55284da45412bf33f9
Kesav928 <Kesav928@gmail.com>
2024-05-02 21:33:22 +0530

    Dictionary Changes

--- datatypes/dictionary_datatype.py
+++ datatypes/dictionary_datatype.py
@@ -8,4 +8,8 @@ d.update({3:'Anand',4:'Chaintanya'}) #Update method to add elements to dict
 print('Data after update',d)
 
 d2={1:'Kesav',1:'Bala',1:'Chaitu'}
+
+d2.update({2:('Kesav','Bala','Chaitu')})
 print(d2)
+# d2[2][1]='Raghav'# we can't override tuple values
+print(d2[2][1])
