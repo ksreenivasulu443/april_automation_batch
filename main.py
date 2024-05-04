195a1c0c7898b85053e6939a746e424fe436ce5c
Bala Anand <balaanand.etl@gmail.com>
2024-05-04 16:48:16 +0530

    Hi

--- main.py
+++ main.py
@@ -15,18 +15,46 @@ project_path = os.getcwd()
 
 postgre_jar = project_path + "/jars/postgresql-42.2.5.jar"
 snow_jar = project_path + "/jars/snowflake-jdbc-3.14.3.jar"
-#oracle_jar = project_path + "/jars/ojdbc11.jar"
 
 jar_path = postgre_jar+','+snow_jar
-spark = SparkSession.builder.master("local[5]") \
-    .appName("test") \
-    .config("spark.jars", jar_path) \
-    .config("spark.driver.extraClassPath", jar_path) \
-    .config("spark.executor.extraClassPath", jar_path) \
-    .getOrCreate()
+
+template_path = project_path + '/config/Master_Test_Template.xlsx'
+test_cases = pd.read_excel(template_path)
+
+print ("test_cases",test_cases)
+
+cwd = os.getcwd()
+#print(cwd)
+# user = os.environ.get('USER')
+# print(user)
+result_local_file = cwd+'\Execution_detailed_summary.txt'
+print("result_local_file",result_local_file)
+
+Out = {
+    "validation_Type": [],
+    "Source_name": [],
+    "target_name": [],
+    "Number_of_source_Records": [],
+    "Number_of_target_Records": [],
+    "Number_of_failed_Records": [],
+    "column": [],
+    "Status": [],
+    "source_type": [],
+    "target_type": []
+}
+run_test_case = test_cases.loc[(test_cases.execution_ind == 'Y')]
+
+# validation = (run_test_case.groupBy('source', 'source_type',
+#                                     'source_db_name', 'source_schema_path', 'source_transformation_query_path',
+#                                     'target', 'target_type', 'target_db_name','target_schema_path',
+#                                     'target_transformation_query_path',
+#                                     'key_col_list', 'null_col_list','exclude_columns',
+#                                     'unique_col_list','dq_column','expected_values','min_val','max_val').
+#               agg(collect_set('validation_Type').alias('validation_Type')))
 #
+# print (validation)
 
-
+validations = validation.collect()
 print("*"*50)
-print(postgre_jar)
-print(jar_path)
+print(f"Execution has started")
+print("*"*50)
