d1ce75c84b5d734d3f9552abbe978563542d88a5
HP <khushbookri.engg15@gmail.com>
2024-05-04 16:10:49 +0530

    commit

--- Collection Datatypes/List.py
+++ Collection Datatypes/List.py
@@ -2,22 +2,6 @@
     created by khushboo on 04/05/2024"""
 
 
-# List(): is used to store the sequence of various types of data. this data type is Mutable
-
-# we want to represent a group of values as single entity where insertion order required to preserve and duplicates are
-# allowed we should go for list data type
-
-'''
-* sequence of various types of data, means --- Heterogeneous objects are allowed
-* insertion order required to preserve
-* Duplicates values are allowed
-* Mutable --- Append, insert, delete, update the value
-* indexing applicable
-* slicing applicable
-* Values should be enclosed by square bracket []
-['append', 'clear', 'copy', 'count', 'extend', 'index', 'insert', 'pop', 'remove', 'reverse', 'sort']
-
-
 ls = [1, 2.0, True, 1+2j, 'string', (1,2),[1,2]]
 
 print("list values", ls)
---
+++ Collection Datatypes/Tuple.py
@@ -0,0 +1,39 @@
+"""This module file is create to practice python Tuple datatypes and
+    created by khushboo on 04/05/2024"""
+
+import sys
+tuple1 = (1, 2.0, True, 1+2j, 'string')
+
+print("tuple1 values", tuple1)
+print("type of tuple1 is ", type(tuple1))
+print("memory of tuple1", id(tuple1))
+
+print("first element in tuple1 is ", tuple1[0], type(tuple1[0]),id(tuple1[0]) )
+print("second element in tuple1 is ", tuple1[1],type(tuple1[1]),id(tuple1[1]))
+
+print("tuple1[0:2]",tuple1[0:2] )
+
+print("methods available in tuple", dir(tuple1))
+
+print("tuple1.index(2.0)",tuple1.index(2.0))
+
+ls = [1,2,3,4,5]
+t =(1,2,3,4,5)
+
+
+
+# print("list size",sys.getsizeof(ls))
+#
+#
+# print("tuple size",sys.getsizeof(t))
+#
+# # print("tuple")
+# # for i in t:
+# #     print(i, sys.getsizeof(i))
+#
+# # for i in ls:
+# #     print(i, sys.getsizeof(i), sys.getrefcount(i))
+#
+#
+# # a= 1002+1j
+# # print(a, sys.getsizeof(a), sys.getrefcount(a)-1)
