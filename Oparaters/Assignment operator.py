cc9ea04755becc86b34b4631b43ee19b664b66bc
lina <lina.dhage31@gmaol.com>
2024-05-07 10:31:34 +0530

    initial commit

---
+++ Oparaters/Assignment operator.py
@@ -0,0 +1,54 @@
+"""this file is created to practice python assignment opearators
+Created by Lina on 06/05/2024"""
+a = 10
+print("a value is", a)
+
+a = b = c = 15
+a = 15
+b = 15
+c = 15
+print("value of a,b,c respectively ", a, b, c)
+
+x, y, z = 11, 12, 13
+print('x,y,z of values respe..', x, y, z)
+
+x, y, z, c = 20, 30.33, 0, 'True'
+# print("values of x,y,z,c",x,y,z,c)
+print("x", x)
+print("y", y)
+print("z", z)
+print('c', c)
+l, m, n = (1, 2, 3), [4, 5, 6], {'str', 'lina', 20.0}
+print("l", l, type(l))
+print("m", m, type(m))
+print("n", n, type(n))
+
+x = y = z = 10
+print("x  type and id is ", x, type(x), id(x))
+print("y type and id is ", y, type(y), id(y))
+print("z type and id is ", z, type(z), id(z))
+
+d = 10
+print("d value is ", d)
+# d = d+2
+#print("d value after the addition ",d)
+d += 2
+print("d value after d+=2 ", d)
+e = 5
+print("e value is ", e)
+e = e - 2
+print("e value after e = e-2 ", e)
+
+# k = 'Lina'
+k = 9
+print("value of k ", k)
+# k= k*4
+k *= 7
+print("value of k after K*4 ",k)
+
+
+
+
+
+
+
