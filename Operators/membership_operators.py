415f0a30c4fc5e8405fa480755c08cfff064f74e
Prashant_C <etldev2023@gmail.com>
2024-05-07 18:05:28 +0530

    This module file is create to practice python All_operators and    created by Prashant on 07/05/2024

---
+++ Operators/membership_operators.py
@@ -0,0 +1,28 @@
+"""This module file is create to practice python membership_operators and
+   created by Prashant on 07/05/2024"""
+
+# Membership_operators
+# In
+# Not in
+
+a = (1,2,3)
+print("check 1 is present in the given variable :",1 in a)
+print("check 2 is present in the given variable :",2 in a)
+print("check 3 is present in the given variable :",3 in a)
+print("check 5 is present in the given variable :",5 in a)
+
+b = "Prashant C"
+print("check P is present in the given variable :",'P' in b)
+print("check p is present in the given variable :",'p' in b)
+
+c = "etldev2023@gmail.com"
+print("check @ is present in the given variable :",'@' in c)
+print("check com is present in the given variable :",'com' in c)
+print("check 2023 is present in the given variable :",'2023' in c)
+print("check 2023 is present in the given variable :",'2024' in c)
+
+
+
+
+
+
