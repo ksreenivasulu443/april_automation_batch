bfd8c270ac37da4c5c193b74ccbc10690d15502d
mahab <a@gmail.com>
2024-05-08 09:07:59 +0530

    if stmt

---
+++ Conditional stmt/if-elif-else-stmt.py
@@ -0,0 +1,65 @@
+#this file is created to practice if-elif-else
+
+#if
+#if-elif
+#if-elif-else
+#if-else
+
+# name=("Mehboob")
+#
+# if name == 'Mehboob':
+#     print("this is inside stmt")
+#     print(f"Hello {name},good morning")
+#
+# print("this is outside stmt")
+
+
+# name = input("enter input name:")
+#
+# if name == 'Mehboob':
+#     print("this is inside stmt")
+#     print(f"Hello {name},good morning")
+# elif name == 'basha':
+#     print("this is inside elif stmt")
+#     print(f"hello {name},good morning and have a nice day")
+# elif name == 'zoya':
+#     print("this is inside elif stmt")
+#     print(f"hello {name},good morning and have a nice day")
+# else:
+#     print("hello guest good morning,welcome to python world"
+
+
+#0-zero,1-one,2-two...10=ten
+
+number =int(input("enter number"))
+print("type of number:",type(number))
+if number==0:
+    print("entered number is zero")
+elif number==1:
+    print("entered number is one")
+elif number==2:
+    print("entered number is two")
+elif number==3:
+    print("entered number is three")
+elif number==4:
+    print("entered number is four")
+elif number==5:
+    print("entered number is five")
+elif number==6:
+    print("entered number is six")
+elif number==7:
+    print("enter number is seven")
+elif number==8:
+    print("enter number is eight")
+elif number==9:
+    print("enter number is nine")
+elif number==10:
+    print("enter number is ten")
+else:
+    print("enter number is not b/t 1..10,please enter right number")
+
+
+
+
+
+
