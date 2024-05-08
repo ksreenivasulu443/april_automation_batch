772f9b170a99cb0ef0cd5dd85cf8ea762df29e49
User <ajayhc135@gmail.com>
2024-05-08 09:31:46 +0530

    Initial commit

---
+++ Assignment/Control_flow.py
@@ -0,0 +1,50 @@
+
+num = int(input("enter the number:"))
+
+if num>0:
+    print("num  is positive", num)
+elif num==0:
+    print("num is Zero", num)
+else:
+    print("num is negative", num)
+
+# Write code age using input method print age >= 18 as eligible for vote ,
+# <18 print not eligible for vote
+# age = int(input("enter the number:"))
+#
+# if age>=18:
+#     print("you are eligible for vote", age)
+# elif age+1==18:
+#     print("you are eligible for vote in next year", age)
+# elif age+2==18:
+#     print("you are eligible for vote in two year")
+# else:
+#     print("you are not eligible for vote", age)
+
+# Write code age using input method if marks >=70 print grade A
+# elif marks 60 to 70 b print grade b
+# elif marks 50 to 60 c print grade c
+# elif marks 40 to 50 d print grade d
+# marks = int(input("enter the marks:"))
+#
+# if marks >= 70:
+#     print("Passed with grade A")
+# elif 60 <= marks < 70:
+#     print("Passed with grade B")
+# elif 50 <= marks < 60:
+#     print("Passed with grade C")
+# elif 40 <= marks < 50:
+#     print("Passed with grade D")
+# else:
+#     print("Failed getting marks lesser than 40")
+
+# palindrome number
+num = 1221
+num =str(num)
+
+# print(num[::-1])
+
+str0 = ''.join(reversed(num))
+
+
+
