0397960c40d899478f63ad6d8aa05643465d353a
mahab <a@gmail.com>
2024-05-10 07:22:03 +0530

    if stmt

--- Conditional stmt/if-elif-else-stmt.py
+++ Conditional stmt/if-elif-else-stmt.py
@@ -58,18 +58,402 @@
 # else:
 #     print("enter number is not b/t 1..10,please enter right number")
 
-number =int(input("enter number"))
-
-print("type of number:",type(number))
-
-if number==0:
-    print("entered number is zero")
-elif number>0:
-    print("print positive number")
-
-else:
-    print("negative number you have enter")
-
-
+# number =int(input("enter number:"))
+#
+# print("type of number:",type(number))
+#
+# if number==0:
+#     print("you have entered number is zero")
+# elif number>0:
+#     print("you have printed positive number")
+#
+# else:
+#     print("negative number you have enter")
 
 
+#print Gaagee is 18
+
+#age =int(input("enter age:"))
+
+#print("type of number:",type(number))
+
+# if age>=18:
+#     print("you are eligible for vote")
+# elif age<18:
+#
+#     print(F"you are eligible for vote after {18-age} years")
+
+
+#print garade marks
+
+# marks = int(input("enter marks details:"))
+#
+# if marks >= 85:
+#     print("outstanding results")
+# #if marks>=70 and marks<60
+# elif 70 <= marks < 60:
+#     print("Grade A")
+# elif 60 <= marks < 70:
+#     print("Grade B")
+# elif 50 <= marks < 60:
+#     print("Grade C")
+# elif 40 <= marks < 50:
+#     print("Grade D")
+# else:
+#     print("Failed in exams")
+
+
+#-----------
+# str='basha'
+#
+# print("reverse string",str[::-1])
+# rev_str = str[::-1]
+#
+# if str ==rev_str:
+#     print("palimdrome")
+# else:
+#     print("not a palimdrome")
+#
+# #----------
+# """this file is created to practice if-elif-else"""
+
+# if
+# if-elif
+# if-elif-else
+# if-else
+
+##############################################
+# Syntax for if
+
+# if condition:
+#     code
+# name = """Sreeni"""
+# # name = input("enter name:")
+# if name == '''Sreeni''':
+#     print("this is inside if statement")
+#     print(f"hello {name}, good morning")
+#
+# print("This is outside if statement")
+
+##############################################
+
+# Syntax: if-elif
+# if condition:
+#     code
+# elif condition:
+#     code
+
+# name = 'sreeni'
+# name = input("enter name:")
+# if name == 'Sreeni' :
+#     print("this is inside if statement")
+#     print(f"hello {name}, Good morning")
+# elif name=='Sreeni' :
+#     print("this is inside elif statement under Raghav")
+#     print(f"hello {name}, Good morning")
+# elif name == 'Sudheer':
+#     print("this is inside elif statement under Sudheer ")
+#     print(f"hello {name}, Good morning")
+#
+# print("This is outside if statement")
+##############################################
+
+
+# sytax : if-elif-else
+# if condition:
+#     code
+# elif condition:
+#     code
+# else:
+#     code
+
+# name = input("enter name:")
+# if name == 'Sreeni':
+#     print("this is inside if statement")
+#     print(f"hello {name}, Good morning")
+# elif name == 'Raghav':
+#     print("this is inside elif statement under Raghav")
+#     print(f"hello {name}, Good morning")
+# elif name == 'Sudheer':
+#     print("this is inside elif statement under Sudheer ")
+#     print(f"hello {name}, Good morning")
+# else:
+#     print("Hello Guest, Good morning")
+#
+# print("this is outside")
+# print("this is outside 2")
+
+
+# 0-Zero, 1-One, 2-two,3 - three, ...10-ten
+#
+# number = int(input("enter number:"))
+# print("type of number", type(number))
+#
+# if number==0 :
+#     print("entered number is Zero")
+# elif number == 1:
+#     print("entered number is one")
+# elif number == 2:
+#     print("entered number is two")
+# elif number == 3:
+#     print("entered number is three")
+# elif number == 4:
+#     print("entered number is four")
+# elif number == 5:
+#     print("entered number is five")
+# elif number == 6:
+#     print("entered number is six")
+# elif number == 7:
+#     print("entered number is seven")
+# elif number == 8:
+#     print("entered number is eight")
+# elif number == 9:
+#     print("entered number is nine")
+# elif number == 10:
+#     print("entered number is ten")
+# else:
+#     print("entered number is not between 0 and 10, please retry")
+
+# take a number using input method and
+# print if num>0 as positive number,
+# elif number = 0 print as zero
+# else negative number
+
+# take age using input method and
+# print if age>=18 as eligible for vote
+# else print not eligible and you will be eligible for vote in x years ( x should 18-entered age)
+
+# take marks using input method
+# print if marks>=70 as Grade A
+# print if marks between 60 and 70 Grade B
+# print if marks between 50 and 60 Grdae C
+# print if marks between 40 and 50 Grdae D
+# else failed
+
+
+
+
+
+
+
+
+
+
+# age = 12  # int(input("enter age:"))
+#
+# if age > 18:
+#     print("Eligible for vote")
+# else:
+#     print("Not eligible")
+#     eligible_for_vote_in_years = 18 - age
+#     print("you will be eligible for vote in ", eligible_for_vote_in_years, "years")
+#
+# number = 10
+#
+# if number > 0:
+#     print("number is positive")
+# else:
+#     print("number is negative")
+#
+# age = 25
+#
+# # Transform age category
+# if age < 25:
+#     category = "Young"
+# elif 25 <= age < 30:
+#     category = "Middle-aged"
+# else:
+#     category = "Elderly"
+#
+# print(f"Age: {age}, Category: {category}")
+#
+# marks = 55
+#
+# if marks>=70:
+#     print("Grade A")
+# elif 60 <= marks < 70:
+#     print("Grade B")
+# elif 50 <= marks < 60:
+#     print("Grade C")
+# elif 40 <= marks < 50:
+#     print("Grade D")
+# else:
+#     print("Fail")
+#
+# str1 = "sreeni"
+# print("reverese string", str1[::-1])
+# rev_str= str1[::-1]
+#
+#
+# if str1 == rev_str:
+#     print("palindrome")
+# else:
+#     print("not palindrome")
+
+# num = '1bb1'
+# print("type of num", type(num))
+# num = str(num)
+# rev_num = num[::-1]
+# print("rev_num", rev_num)
+#
+# if num == num[::-1]:
+#     print("palindrome")
+# else:
+#     print("not palindrome")
+
+
+#
+# #count, file_read, positive or negative
+#
+# a = 10
+# b = 10
+# c = -10
+# if a > 0 and b > 0:
+#     print("The numbers are greater than 0")
+# if a > 0 and b > 0 and c > 0:
+#     print("The numbers are greater than 0")
+# else:
+#     print("Atleast one number is not greater than 0")
+#
+#
+#
+#
+# # Python program to demonstrate
+# # logical and operator
+# a = 10
+# b = 12
+# c = 0
+# if a and b and c:
+#     print("All the numbers have boolean value as True")
+# else:
+#     print("Atleast one number has boolean value as False")
+#
+
+#etl project , source count, target
+
+# source_count = 10
+# target_count = 9
+# if source_count == target_count:
+#     print("count is matching")
+# else:
+#     print("count is not matching and difference is", abs(source_count-target_count))
+#
+#
+# #files--> csv, text, json, avro, parquet, table(oracle, mysql, sqlserver, post, snow)
+# file_type = input("enter file type:")
+# header = False
+# if file_type == 'csv':
+#     print("code to read csv file")
+#     if header :
+#         print("code will read csv file with header")
+#     else:
+#         print("code will read csv file without header")
+# elif file_type == 'json':
+#     print("code to read json file")
+# elif file_type == 'parquet':
+#     print("code to read parquet file")
+# else:
+#     print("entered format is not handled")
+
+#practice
+
+# name = input("enter name:")
+# if name=='Srinee':
+#     print(f"welcome {name} ,good morning have nice day")
+# elif name =='Basha':
+#     print(f"welcome {name} ,good morning have nice day")
+# elif name =='Nafees':
+#     print(f"welcome {name} ,good morning have nice day")
+# elif name =='Zoya':
+#     print(f"welcome {name} ,good morning have nice day")
+# else:
+#     print(f"list of {name} not present ")
+
+#-------
+# number=int(input("enter number:"))
+#
+# if number==0:
+#     print("print zero value")
+# elif number==1:
+#     print("print one value")
+# elif number==2:
+#     print("print two value")
+# elif number==3:
+#     print("print three value")
+# elif number==4:
+#     print("print four value")
+# elif number==5:
+#     print("print five value")
+# else:
+#     print("enter the number is not in between 1 to 5")
+
+#------------
+
+#control conditon ex-7
+# take a number using input method and
+# print if num>0 as positive number,
+# elif number = 0 print as zero
+# else negative number
+
+# num = int(input("enter number:"))
+# if num >0:
+#     print("you have entered +ve numbers")
+# elif num ==0:
+#     print("you have entered zero value")
+# else:
+#     print("you have entered -ve value")
+
+#cost of the bike and tax paid for the bike
+
+# tax=0
+# pr = int(input("enter price of the bike:"))
+# if pr>100000:
+#     tax = 15/100*pr
+# elif pr>50000 and pr<=100000:
+#     tax = 10/100*pr
+# else:
+#     tax=5/100*pr
+# print("tax to be paid",tax)
+
+#write program for days in week
+#
+# num=int(input("enter any number between 1 to 7:-"))
+#
+# if num==1:
+#     print("sunday")
+# elif num==2:
+#     print("monday")
+# elif num==3:
+#     print("Tuesday")
+# elif num==4:
+#     print("Wednsday")
+# elif num==5:
+#     print("Thursday")
+# elif num==6:
+#     print("Friday")
+# elif num==7:
+#     print("Saturday")
+# else:
+#     print("number you have enter is wrong number,please re-check and enter correct num")
+
+
+#find the lowest number from 2 numbers
+# num1=int(input("enter the first number:"))
+# num2=int(input("enter the 2nd number:"))
+# if num1>num2:
+#     print("smaller number is",num2)
+# else:
+#     print("smaller number is",num1)
+
+
+###############
+#Gratest num from 3 numbers
+num1=int(input("Enter first number:"))
+num2=int(input("Enter 2nd number:"))
+num3=int(input("Enter third number:"))
+if num1>num2 and num1>num3:
+    print("Gratest number is ",num1)
+elif num2>num1 and num2>num3:
+    print("Gratest number is ",num2)
+elif num3>num2 and num3>num2:
+    print("Gratest number is ",num3)
