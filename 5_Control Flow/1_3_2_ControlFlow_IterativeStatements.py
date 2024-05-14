9c40f93eb9df00a962c7411d53a37e49580ee164
User <ajayhc135@gmail.com>
2024-05-14 09:44:08 +0530

    Initial commit

--- 5_Control Flow/1_3_2_ControlFlow_IterativeStatements.py
+++ 5_Control Flow/1_3_2_ControlFlow_IterativeStatements.py
@@ -6,3 +6,30 @@ should go for while loop.
 while condition :
  body
 '''
+print("increasing while loop")
+i = 0
+while i<=10: # infinite loop
+    print(i)
+    i = i+1  # range control
+
+print("decreasing while loop")
+i = 10
+while i>=0: # infinite loop
+    print(i)
+    i = i-1  # range control
+
+# list iteration
+print("----------list iteration----------")
+ls = [10, 20, 30, 40, 50]
+i=0
+while i < len(ls):
+    print(ls[i])
+    i +=1
+
+ls1 = ["Apple", "Banana", "Coconut"]
+i = 0
+while i <= len(ls1):
+    if i == "Banana":
+        break
+        # print(ls1[i])
+    i=i+1
