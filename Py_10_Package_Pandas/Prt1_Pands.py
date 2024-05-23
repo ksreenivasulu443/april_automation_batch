4c75b3b3eda66a7d542a95c3eca9ab617baefe4c
User <ajayhc135@gmail.com>
2024-05-23 10:15:29 +0530

    Initial commit

--- Py_10_Package_Pandas/Prt1_Pands.py
+++ Py_10_Package_Pandas/Prt1_Pands.py
@@ -15,7 +15,13 @@ rec = pd.read_sql('SELECT * FROM test_data', conn)
 print(cnt)
 print(rec)
 
-df2 = pd.DataFrame( ([1, "Arun", 'B+'],[2, "Babu", 'A'],[3, "Chiru", 'B+']), columns=[ "sno", "Student_name", "Grade"])
+df2 = pd.DataFrame( ([1, "Arun", 'B+'],[2, "Babu", 'A'],[3, "Chiru", 'B+']),
+                    columns=[ "sno", "Student_name", "Grade"], index=['a','b','c'])
 print(df2)
 
 # print(help(pd.DataFrame))
+
+ser = pd.Series([1, 2, 3], index=["a", "b", "c"])
+df3 = pd.DataFrame(data=ser)
+print("series converted as df")
+print(df3)
--- Py_9_Numpy/Prt1_1_Numpy_arry.py
+++ Py_9_Numpy/Prt1_1_Numpy_arry.py
@@ -10,12 +10,13 @@ print("vale of arr[0] and type of arr[0]", arr[1][5], type(arr[1][5]))
 series = pd.Series([1, 2, 3, 4], index=['a', 'b', 'c', 'd'])
 
 print("type of series is", type(series))
-
 print("value of b index", series['b'])
-
-
+print("type of data in series", series.dtypes)
+print("all methods available", dir(series))
 
 arr1 = np.array([[1, 2, 3, 4, 5],['Ajay', 'Baskar', 'Chandan', 'Deva', 'Esha']])
 print("type of arr is", type(arr1))
 print("vale of arr[0] and type of arr[0]", arr1[1][2], type(arr1[1][2]))
 print("vale of arr1", arr1)
+
+
