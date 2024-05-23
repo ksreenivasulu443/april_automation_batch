cc36581cadf03bb20d807ba16aa5283ac62aee5c
User <ajayhc135@gmail.com>
2024-05-23 10:09:14 +0530

    Initial commit

---
+++ Py_10_Package_Pandas/Prt1_Pands.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sqlite4 import SQLite4
+from sqlite3 import connect
+
+
+conn = connect(':memory:')
+data = {'Name': ['Alice', 'Bob', 'Charlie'],'Age': [25, 30, 35],
+        'City': ['New York', 'Los Angeles', 'Chicago']}
+
+df = pd.DataFrame(data)
+print(df)
+df.to_sql(name='test_data', con=conn)
+cnt = pd.read_sql('SELECT count(*) FROM test_data', conn)
+rec = pd.read_sql('SELECT * FROM test_data', conn)
+print(cnt)
+print(rec)
+
+df2 = pd.DataFrame( ([1, "Arun", 'B+'],[2, "Babu", 'A'],[3, "Chiru", 'B+']), columns=[ "sno", "Student_name", "Grade"])
+print(df2)
+
+# print(help(pd.DataFrame))
