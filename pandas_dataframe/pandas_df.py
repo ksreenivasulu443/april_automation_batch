7ef402abda2953ad2d0ec9b742ecb38406071806
bbhoi <babulubh358@gmail.com>
2024-05-23 22:16:33 +0530

    updated changes

--- pandas_dataframe/pandas_df.py
+++ pandas_dataframe/pandas_df.py
@@ -1,15 +1,21 @@
+#
+# import pandas as pd
+# data = {'Name': ['Alice', 'Bob', 'Charlie'],'Age': [25, 30, 35],
+#         'City': ['New York', 'Los Angeles', 'Chicago']}
+# df = pd.DataFrame(data)
+# print(df)
+#
+# print(type(data))
+#
+# print(help(pd.DataFrame(data)))
+#
+# df2=pd.DataFrame([11,12,13,14,25],columns=['sno'], index=['a','b','c','d','e'])
+# print(df2)
+# print(help(pd.DataFrame))
 
 import pandas as pd
-data = {'Name': ['Alice', 'Bob', 'Charlie'],'Age': [25, 30, 35],
-        'City': ['New York', 'Los Angeles', 'Chicago']}
-df = pd.DataFrame(data)
-print(df)
 
-print(type(data))
+csv_read_df=pd.read_csv(r'C:\Users\bbhoi\PycharmProjects\datapythonautomation\Files\Contact_info.csv')
+print(csv_read_df)
 
-print(help(pd.DataFrame(data)))
 
-df2=pd.DataFrame([11,12,13,14,25],columns=['sno'], index=['a','b','c','d','e'])
-print(df2)
-
-=pd.read_csv()
