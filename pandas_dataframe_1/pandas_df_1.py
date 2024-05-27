7b3011c680d43812e0d7dc5a1ce2007adef86b94
Prashant_C <etldev2023@gmail.com>
2024-05-27 08:13:51 +0530

    .

--- pandas_dataframe_1/pandas_df_1.py
+++ pandas_dataframe_1/pandas_df_1.py
@@ -1,16 +1,16 @@
+import pandas as pd
+from pandasql import sqldf
 
 # •import pandas as pd
 # •data = {'Name': ['Alice', 'Bob', 'Charlie'],'Age': [25, 30, 35],'City': ['New York', 'Los Angeles', 'Chicago’] }
 # df= pd.DataFrame(data)
 # print(df)
 
-import pandas as pd
 data = {'Name': ['Alice', 'Bob', 'Charlie'],'Age': [25, 30, 35],
         'City': ['New York', 'Los Angeles', 'Chicago']}
 df = pd.DataFrame(data)
 print(df)
 
-import pandas as pd
 data = {'Name': ['Alice', 'Bob', 'Charlie'],'Age': [25, 30, 35],
         'City': ['New York', 'Los Angeles', 'Chicago']}
 df = pd.DataFrame(data)
