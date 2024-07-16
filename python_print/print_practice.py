205d3aee295f0fa7beb50fe2247dad4fd9bcbda1
Prashant_C <etldev2023@gmail.com>
2024-07-16 22:50:33 +0530

    .

--- python_print/print_practice.py
+++ python_print/print_practice.py
@@ -6,45 +6,50 @@ import sys
 import time
 
 # Variable creation
-a = 10
-b = '10'
-c = 'sreeni'
-d = 10.5
+# a = 10
+# b = '10'
+# c = 'sreeni'
+# d = 10.5
+#
+# #Priniting type of variables using print function and type function
+# print("type of a ", type(a))
+# print("type of b ", type(b))
+# print("type of c ", type(c))
+# print("type of d ", type(d))
+#
+# print("type of [10,'10', 'sreeni',10.5] ", type([10,'10', 'sreeni',10.5]))
+#
+# e = 10
+#
+# print("memory of a ",id(a))
+# print("memory of e ",id(e))
+#
+# f = 10
+# print("memory of f ",id(f),id(e), end='\n',sep='-')
+# print("memory of d ",id(d))
+# print("memory of b ",id(b))
+#
+#
+# # writing output to file
+# file = open('test.txt', 'a')
+# original = sys.stdout
+# sys.stdout = file
+#
+# print(a,b, sep=' ', end='\n', file=file, flush=False)
+# print(a,b, flush=False)
+# print(a,b, flush=False)
+# print(a,b,c ,flush=False)
+#
+# name = 'Prashant'
+# age = 20
+#
+# print(f"name is {name} and age is {age}")
+#
+# print("name is ", name, " and age is ", age, sep='')
 
-#Priniting type of variables using print function and type function
-print("type of a ", type(a))
-print("type of b ", type(b))
-print("type of c ", type(c))
-print("type of d ", type(d))
 
-print("type of [10,'10', 'sreeni',10.5] ", type([10,'10', 'sreeni',10.5]))
+z = 3107
+print(z)
 
-e = 10
-
-print("memory of a ",id(a))
-print("memory of e ",id(e))
-
-f = 10
-print("memory of f ",id(f),id(e), end='\n',sep='-')
-print("memory of d ",id(d))
-print("memory of b ",id(b))
-
-
-# writing output to file
-file = open('test.txt', 'a')
-original = sys.stdout
-sys.stdout = file
-
-print(a,b, sep=' ', end='\n', file=file, flush=False)
-print(a,b, flush=False)
-print(a,b, flush=False)
-print(a,b,c ,flush=False)
-
-name = 'Prashant'
-age = 20
-
-print(f"name is {name} and age is {age}")
-
-print("name is ", name, " and age is ", age, sep='')
-
-
+n = 'Nivi'
+print(n)
