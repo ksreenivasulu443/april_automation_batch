263e4e6e7968abfc57d0aa41c784c7ad57fb72ab
Shamim <shaik.etl14@gmail.com>
2024-07-30 04:05:58 -0700

    Hi

--- datatypes/Practice.py
+++ datatypes/Practice.py
@@ -8,3 +8,36 @@ for i in range(10):
 else:
     var+=1
 print(var)
+
+################################################
+str1 = "PYnative"
+print(str1[1:4], str1[:5], str1[4:], str1[0:-1], str1[:-1])
+####################################################
+str1 = 'Welcome'
+print (str1[:6] + ' PYnative')
+
+str = "my name is James bond";
+print (str.capitalize())
+
+st1 = "pynative"
+st2 = "pynative"
+print(st1 == st2)
+print(st1 is st2)
+
+str1 = "My salary is 7000";
+str2 = "7000"
+
+print(str1.isdigit())
+print(str2.isdigit())
+
+str = "My salary is 7000";
+print(str.isalnum())
+
+
+str1 = "my isname isisis jameis isis bond";
+sub = "is";
+print(str1.count(sub, 4))
+
+
+
+
