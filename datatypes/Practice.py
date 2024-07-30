c7b1170decac0aa4d44d13d0e54f3086260fd884
Shamim <shaik.etl14@gmail.com>
2024-07-30 06:11:05 -0700

    Hi

--- datatypes/Practice.py
+++ datatypes/Practice.py
@@ -39,5 +39,57 @@ sub = "is";
 print(str1.count(sub, 4))
 
 
+aList = [5, 10, 15, 25]
+print(aList[::-2])
+
+sampleList = [10, 20, 30, 40, 50]
+print(sampleList[-2])
+print(sampleList[-4:-1])
+
+resList = [x+y for x in ['Hello ', 'Good '] for y in ['Dear', 'Bye']]
+print(resList)
+
+
+aList = [4, 8, 12, 16]
+aList[1:4] = [20, 24, 28]
+print(aList)
+
+sampleList = [10, 20, 30, 40, 50]
+sampleList.pop()
+print(sampleList)
+
+sampleList.pop(2)
+print(sampleList)
+
+list1 = ['xyz', 'zara', 'PYnative']
+print (max(list1))
+
+sampleList = [10, 20, 30, 40]
+del sampleList[0:6]#deleting all  all elements
+print(sampleList)
+
+l = [None] * 10
+print(len(l))
+
+aList = [10, 20, 30, 40, 50, 60, 70, 80]
+print(aList[2:5])
+print(aList[:4])
+print(aList[3:])
+
+my_list = ["Hello", "Python"]
+print("-".join(my_list))
+
+sampleList = [10, 20, 30, 40, 50]
+sampleList.append(60)
+print(sampleList)
+
+sampleList.append(60)
+print(sampleList)
+
+aList = ["PYnative", [4, 8, 12, 16]]
+print(aList[0][1])
+print(aList[1][3])
+
+
 
 
