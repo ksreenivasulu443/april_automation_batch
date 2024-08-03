0b209f504c9ae2738f8c2926931b91b332612340
Prashant_C <etldev2023@gmail.com>
2024-08-03 08:52:32 +0530

    .

--- datatypes/complex.py
+++ datatypes/complex.py
@@ -1,21 +1,45 @@
-"""This module file is create to practice python complex datatypes
-   created by Prashant on 04/23/2024"""
-
-
-# 2 real, 1 imagi, j = sqrt(-1))
-c = 2.0 + 1j
+# """This module file is create to practice python complex datatypes
+#    created by Prashant on 04/23/2024"""
+#
+#
+# # 2 real, 1 imagi, j = sqrt(-1))
+# c = 2.0 + 1j
+# print("The value of c is", c)
+# print("Type of c is", type(c))
+# print("Memory address of c is", id(c))
+# d = 1j
+# print("The value of d is", d)
+# print("Type of d is", type(d))
+# print("Memory address of d is", id(d))
+# e = 0.1 + 1j
+# print("The value of e is", e)
+# print("Type of e is", type(e))
+# print("Memory address of e is", id(e))
+# print("methods available:", dir(e))
+# print(f"Imaginary value {e}", e.imag)
+# print(f"real value{e}", e.real)
+#
+c = 2 + 2j
 print("The value of c is", c)
 print("Type of c is", type(c))
 print("Memory address of c is", id(c))
-d = 1j
-print("The value of d is", d)
-print("Type of d is", type(d))
-print("Memory address of d is", id(d))
-e = 0.1 + 1j
-print("The value of e is", e)
-print("Type of e is", type(e))
-print("Memory address of e is", id(e))
-print("methods available:", dir(e))
-print(f"Imaginary value {e}", e.imag)
-print(f"real value{e}", e.real)
 
+c_1 = 2j
+print("The value of c_1 is", c_1)
+print("Type of c_1 is", type(c_1))
+print("Memory address of c_1 is", id(c_1))
+
+c_2 = 2
+print("The value of c_2 is", c_2)
+print("Type of c_2 is", type(c_2))
+print("Memory address of c_2 is", id(c_2))
+
+c_3 = 0.2 + 2j
+print("The value of c_3 is", c_3)
+print("Type of c_3 is", type(c_3))
+print("Memory address of c_3 is", id(c_3))
+
+c_4 = 1 + 2j
+print("functions available in float type", dir(c_4))
+print("Imaginary value of c_4 is :", c_4.imag)
+print("Real value of c_4 is :", c_4.real)
